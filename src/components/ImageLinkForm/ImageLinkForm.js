ef3c9e09839ba6f48240d25821f65edc81ef2497
futuregit <gmail.com>
2019-12-30 01:53:52 -0500

    Updated components

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
-import './Logo.css';
 const ImageLinkForm = () => {
     return (
        <div>    
            <p className='f3'>
                { 'This Thinker will detect faces in your pictures. Give it a try' }
            </p>
+           <div>
+               <input className='f4 pa2 w-70 center' type='text' />
+               <button>Detect</button>
+           </div> 
        </div>
     );
 }
