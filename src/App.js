bdedec33e438bd2a063c58e752b891137c2843e4
futuregit <futuregit@users.noreply.github.com>
2019-12-29 19:14:54 -0500

    Added Navigation component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Navigation from './Components/Navigation/Navigation';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <Logo />
+      {/*<Logo />
+      <ImageLinkForm />
+      <FaceRecognition />*/}
     </div>
   );
 }
